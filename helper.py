0779d66210d151a963170409ea7a2d9f8fc6c8a5
maxdickinson <max.dickinson58@gmail.com>
2016-12-31 17:23:23 -0800

    more oop and text added

--- helper.py
+++ helper.py
@@ -14,8 +14,9 @@ class Utility:
     #smorgasboard of assisting functions
     def __init__(self):
         #set the number of ms to wait as a delay
-        self.ms = 50
-
+        #controls the animatin speed, not the walk speed
+        self.ms = 1
+        self.movement_speed = 1
     def speed(self):
         #wrapper
         return pygame.time.wait(self.ms)
@@ -95,32 +96,13 @@ class TextBlock(Utility):
 
 
 
-def walkOn(screen,cls,my_group,c):
-    #block the sprite and text, can somehow combine group and c
-    for i in cls:
-        screen.blit(i.cover, i.rect)
-    screen.blit(c.cover, c.rect)
-    # update
-    # a.update()
-    for i in cls:
-        i.update()
-
-    my_group.update()
-    my_group.draw(screen)
-
-    for i in cls:
-        screen.blit(i.tex, i.rect)
-    # update screen
-    pygame.display.update()
-    pygame.display.flip()
-
 def createText(x,y,text,cutoff):
     #make the text blocks
     b=TextBlock(0,0,text)#not doing xy
     cls = b.wraptext(x,y,text,cutoff)
     return cls
 
-#cls2 = b.wraptext(10,10, " this is also max)
+
 def create(cls):
     #create the sprites
     #for i in cls:
@@ -130,22 +112,6 @@ def create(cls):
     c = TestSprite(cls[0].rect.left-w,cls[0].rect.top,w,h) # linking by width
     return c
 
-# make all the text boxes
-cls=createText(0,0,""" Max Dickinson 503-956-3213 max.dickinson58@gmail.com Github: maxdickinson""" , 100)
-cls2 = createText(0,cls[-1].rect.bottom +5,"""Education University of Oregon (Sep. 2011 – Dec. 2016) Applied Physics and Math-Computer Science double major""" , 200)
-cls3 = createText(0,cls2[-1].rect.bottom +5,"""Technical skills: Python, R, Javascript, Matlab C, C++ SQL Git Microsoft Office Suite""" , 200)
-cls4 = createText(0,cls3[-1].rect.bottom +5,"""Work experience Math Research Assistant (April 2016 – June 2016) Developed a pipeline in R to analyze mixed data and explored different R packages related to clustering and visualization for inter-university NSF project about clustering with applications for mental health. Use multiple packages to cluster and visualize data""" , 200)
-cls5 = createText(0,cls4[-1].rect.bottom +5,"""Tutor (July 2015 – December 2016) Taught math and physics of all levels to over 50 high school and college students.""" , 200)
-cls6 = createText(0,cls5[-1].rect.bottom +5,"""Paper Marker, University of Oregon Mathematics Department (July 2014 – July 2016) Graded papers for calculus, differential equations, elementary linear algebra, and discrete dynamical systems. Communicated progres and results with professors""" , 200)
-cls7 = createText(0,cls6[-1].rect.bottom +5,"""Biophysics Research Assistant (April 2013 – May 2013)Used an optical trap to pull pieces of a cell membrane which was collected and analyzed in Matlab. """ , 200)
-cls8 = createText(0,cls7[-1].rect.bottom +5,"""Physics Research Assistant (June 2012 – Nov. 2013) Built a scanning wavelength program to tune lab equipment """ , 200)
-cls9 = createText(0,cls8[-1].rect.bottom +5,"""Mission: Citizen board member (Sep. 2010 – Sep. 2012) Founding member of a 501c3 nonprofit. Developed and taught civics courses to first generation US immigrants Helped over 10 people pass the citizenship test. """ , 200)
-
-clsss = [cls,cls2,cls3,cls4,cls5,cls6,cls7,cls8,cls9]
-
-creates= list(map(create,clsss))
-
-groups = list(map(pygame.sprite.Group , creates))
 
 #for i in groups:
    # print(i)
@@ -156,46 +122,114 @@ groups = list(map(pygame.sprite.Group , creates))
 pygame.display.update()
 pygame.display.flip()
 
-flags = [0 for i in range(len(clsss))]
-clickcounter=0
-i = 0
-j = 0
-k = 0
+
+
+class gameControl:
+    def __init__(self):
+        #counters for arrays
+        self.i = 0
+        self.j=0
+        self.k =0
+        self.clickcounter=0
+
+        # make all the text boxes
+
+        cls = createText(0, 0, """ Max Dickinson 503-956-3213 max.dickinson58@gmail.com Github: maxdickinson""", 100)
+        cls2 = createText(0, cls[-1].rect.bottom + 5,
+                          """Education University of Oregon (Sep. 2011 – Dec. 2016) Applied Physics and Math-Computer Science double major""",
+                          200)
+        cls3 = createText(0, cls2[-1].rect.bottom + 5,
+                          """Technical skills: Python, R, Javascript, Matlab C, C++ SQL Git Microsoft Office Suite""",
+                          200)
+        cls4 = createText(0, cls3[-1].rect.bottom + 5,
+                          """Work experience Math Research Assistant (April 2016 – June 2016) Developed a pipeline in R to analyze mixed data and explored different R packages related to clustering and visualization for inter-university NSF project about clustering with applications for mental health. Use multiple packages to cluster and visualize data""",
+                          200)
+        cls5 = createText(0, cls4[-1].rect.bottom + 5,
+                          """Tutor (July 2015 – December 2016) Taught math and physics of all levels to over 50 high school and college students.""",
+                          200)
+        cls6 = createText(0, cls5[-1].rect.bottom + 5,
+                          """Paper Marker, University of Oregon Mathematics Department (July 2014 – July 2016) Graded papers for calculus, differential equations, elementary linear algebra, and discrete dynamical systems. Communicated progres and results with professors""",
+                          200)
+        cls7 = createText(0, cls6[-1].rect.bottom + 5,
+                          """Biophysics Research Assistant (April 2013 – May 2013)Used an optical trap to pull pieces of a cell membrane which was collected and analyzed in Matlab. """,
+                          200)
+        cls8 = createText(0, cls7[-1].rect.bottom + 5,
+                          """Physics Research Assistant (June 2012 – Nov. 2013) Built a scanning wavelength program to tune lab equipment """,
+                          200)
+        cls9 = createText(0, cls8[-1].rect.bottom + 5,
+                          """Mission: Citizen board member (Sep. 2010 – Sep. 2012) Founding member of a 501c3 nonprofit. Developed and taught civics courses to first generation US immigrants Helped over 10 people pass the citizenship test. """,
+                          200)
+
+        self.clsss = [cls, cls2, cls3, cls4, cls5, cls6, cls7, cls8, cls9]
+
+        self.creates = list(map(create, self.clsss))
+
+        self.groups = list(map(pygame.sprite.Group, self.creates))
+        self.flags= [0 for i in range(len(self.clsss)+2)]
+
+    def checkDone(self,array,num1,num2):
+        if array[self.i] == num1 and array[self.j] == num2:
+            return True
+    def fromLeft(self,sprite,screen):
+        return sprite[self.k].rect.right < screen.get_rect().width / 2
+
+    def walkOn(self,screen, cls, my_group, c):
+        # block the sprite and text, can somehow combine group and c
+        for i in cls:
+            screen.blit(i.cover, i.rect)
+        screen.blit(c.cover, c.rect)
+        # update
+        # a.update()
+        for i in cls:
+            i.update()
+
+        my_group.update()
+        my_group.draw(screen)
+
+        for i in cls:
+            screen.blit(i.tex, i.rect)
+        # update screen
+        pygame.display.update()
+        pygame.display.flip()
+
+
+gc = gameControl()
 while True:
     #erase
    # screen.blit(a.cover,a.rect)
     for event in pygame.event.get():
+        #print(gc.clickcounter)
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.BUTTON_X1:
+        if event.type == pygame.BUTTON_X1:
 
-            clickcounter +=1
-        elif event.type == pygame.MOUSEBUTTONDOWN and button.collidepoint(pygame.mouse.get_pos()) and flags[i] ==0:
+            gc.clickcounter +=1
+
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and button.collidepoint(pygame.mouse.get_pos()) and gc.k< len(gc.clsss):
+            if gc.checkDone(gc.flags,0,0):
             #thread is locked while this is happening
-            flags[i] = 1
-            j+=1
-            while creates[k].rect.right < screen.get_rect().width/2:
+            #mark done move to next one
+                gc.flags[gc.i] = 1
+                gc.j+=1
+                #movement and how to move
+                while gc.fromLeft(gc.creates,screen):
+                    gc.walkOn(screen,gc.clsss[gc.k],gc.groups[gc.k],gc.creates[gc.k])
+                gc.k+=1
 
-                walkOn(screen,clsss[k],groups[k],creates[k])
-            k+=1
-        elif event.type == pygame.MOUSEBUTTONDOWN and button.collidepoint(pygame.mouse.get_pos()) and clickcounter >10 and flags[i]  == 1 and flags[j] == 0  :
-            flags[i] =1
-            i +=1
-            j +=1
-            while creates[k].rect.right < screen.get_rect().width / 2:
-                walkOn(screen, clsss[k], groups[k], creates[k])
-            k+=1
-        elif event.type == pygame.MOUSEBUTTONDOWN and button.collidepoint(pygame.mouse.get_pos()) and clickcounter >10 and flags[i]  == 1 and flags[j] == 0:
-            flags[i] = 1
-            i += 1
-            j += 1
-            while creates[k].rect.right < screen.get_rect().width / 2:
-                walkOn(screen, clsss[k], groups[k], creates[k])
-            k+=1
-        elif event.type == pygame.MOUSEBUTTONDOWN and button.collidepoint(pygame.mouse.get_pos()) and clickcounter >10 and flags[i]  == 1 and flags[j] == 0:
-            flags[i] = 1
-            i += 1
-            j += 1
-            while creates[k].rect.right < screen.get_rect().width / 2:
-                walkOn(screen, clsss[k], groups[k], creates[k])
-            k+=1
+            elif ((gc.clickcounter % 10) == 2) & gc.checkDone(gc.flags,1,0):
+
+            #thread is locked while this is happening
+            #mark done move to next one
+                gc.flags[gc.j] = 1
+                gc.i+=1
+                gc.j+=1
+                #movement and how to move
+                while gc.fromLeft(gc.creates,screen):
+                    gc.walkOn(screen,gc.clsss[gc.k],gc.groups[gc.k],gc.creates[gc.k])
+
+                gc.k+=1
+                gc.clickcounter +=1
+
+            else:
+                break
