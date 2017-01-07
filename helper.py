599180908147ea5e6c6edbbf0c1a48b73e841658
maxdickinson <max.dickinson58@gmail.com>
2017-01-07 00:02:06 -0800

    two pages

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import os
 import math
-
+import copy
 pygame.init()
 os.chdir("C:\\Users\\max\\Desktop\\pigwalk")
 
@@ -10,9 +10,38 @@ os.chdir("C:\\Users\\max\\Desktop\\pigwalk")
 screen = pygame.display.set_mode((600, 600))
 screen.fill((255, 255, 255))
 
+from colour import Color
+red = Color("black")
+colors = list(red.range_to(Color("red"),10))
+
 
 # initial buttons
 
+class TextBlock:
+    #make a text block that can move, not wrapped yet
+    # doesnt do the blit just the creation
+    def __init__(self,x,y,text):
+        self.fon = pygame.font.SysFont(None, 16)
+        #w,h = self.fon.size(text)
+        w,h = 40,40
+        self.rect = pygame.Rect(x,y,w,h)
+
+        self.tex=self.fon.render(text,True,(0,0,255))
+        #create eraser
+        self.cover = pygame.Surface((w,h))
+        self.cover.fill((5,255,255))
+
+    def update(self):
+        self.rect = pygame.Rect(self.rect.left + 1, self.rect.top , self.rect.h,self.rect.w)
+
+
+
+def createText(x,y,text,cutoff):
+    #make the text blocks
+
+    b=TextBlock(x,y,text)#not doing xy
+    pygame.draw.rect(screen, (0, 0, 0), b.rect)
+    return b
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, file, location):
@@ -22,7 +51,7 @@ class Background(pygame.sprite.Sprite):
 
 
 bgnd1 = Background("background1.png", (0, 0))
-
+bgnd2 = Background("background2.png", (0, 0))
 screen.blit(bgnd1.image, bgnd1.rect)
 
 # 4 RECTANGLES COVERING SCREEN EVENLY
@@ -39,9 +68,7 @@ recs2 = [a, b, c, d]
 
 start = [True] * 4
 repeat = True
-
-
-# circle=[targetingCircle(screen, (int(a[0] + a[2] / 2), int(a[1] + a[3] / 2)), a[2] / 2)]
+cols =  [False] * 4
 
 
 def radpos(a):
@@ -80,7 +107,23 @@ for p in rp:
     py.append(p[1][1])
 
 
+radius = int(a[2] / 2)
+nextBut = createText(300, 300, "Next", 200)
+curr = bgnd1
+colr = 0
 
+class block:
+    def __init__(self,colorin,coord):
+        self.colr =colorin
+        self.coord = coord
+q = block(0,a)
+r = block(0,b)
+s = block(0,c)
+t =  block(0 , d)
+bl = [q,r,s,t]
+bl2 =[q,r,s,t]
+index = 0
+index2 = 0
 while True:
     x, y = pygame.mouse.get_pos()
 
@@ -93,25 +136,66 @@ while True:
         # print(circle[0].collidepoint(pygame.mouse.get_pos()))
         # print(gc.clickcounter
         # print(event.type)
-        print(event)
+
         if event.type == pygame.QUIT:
             sys.exit()
-    for i in range(len(px)):
-        if math.sqrt((px[i] - x) ** 2 + (py[i] - y) ** 2) <= radius and start[i] == True:
-            recs.remove(recs2[i])
-            recs1.remove(recs3[i])
-            start[i] = False
+        elif start[0:4] == [False]*4 and nextBut.rect.collidepoint(pygame.mouse.get_pos()) and (event.type == pygame.MOUSEBUTTONDOWN):
 
+            start = [True] * 4
+            curr = bgnd2
+            recs = [a, b, c, d]
+            bl = [q ,r ,s ,t]
+            recs1 = [a, b, c, d]
+        elif event.type == pygame.MOUSEBUTTONDOWN and curr == bgnd2 and index < 4 :
+            bl2[index].colr +=1
+            if bl2[index].colr == len(colors):
+                cols[index] = True
+                bl2[index].colr =len(colors)-1
+                index +=1
+            #makes the other box change colors first
+    if curr == bgnd1:
+        for i in range(len(px)):
+
+            if math.sqrt((px[i] - x) ** 2 + (py[i] - y) ** 2) <= radius and start[i] ==True :
+                recs.remove(recs2[i])
+                recs1.remove(recs3[i])
+                print(bl)
+                print(bl2[i])
+                bl.remove(bl2[i])
+                start[i] = False
+                #start[i::] == [True] * (4 - i) and start[0:i] == [False] * i
+    elif curr == bgnd2:
+
+
+        if math.sqrt((px[index2] - x) ** 2 + (py[index2] - y) ** 2) <= radius and cols[index2] == True and start[i] == True and index2<4:
+            recs.remove(recs2[index2])
+            recs1.remove(recs3[index2])
+            bl.remove(bl2[index2])
+            start[index2] = False
+            if index2==3:
+                index2 = 3
+            else:
+                index2+=1
+
+
+    # circle=[targetingCircle(screen, (int(a[0] + a[2] / 2), int(a[1] + a[3] / 2)), a[2] / 2)]
 
     # update background image
-    screen.blit(bgnd1.image, bgnd1.rect)
+    screen.blit(curr.image, curr.rect)
+    if len(recs) == 0:
+        nextBut = createText(300, 300, "Next", 200)
+        screen.blit(nextBut.tex,nextBut.rect)
+
     # draw blockers
-    for rec in recs1:
-        pygame.draw.rect(screen, (0, 0, 0), rec)
+    color = (0, 0, 0)
+
+    for rec in bl:
+
+        pygame.draw.rect(screen, (colors[rec.colr].rgb[0]*255 , colors[rec.colr].rgb[1]*255 , colors[rec.colr].rgb[2]*255), rec.coord)
     # shrink circle
 
-    for q in rp:
-        drew2 = [drawCircle2(q, radius)]
+    for oo in rp:
+        drew2 = [drawCircle2(oo, radius)]
     if radius > 4:
         radius -= 3
     else:
