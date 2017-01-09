3c593ae82e76a34407d6cf90d5d5b6d99765f8e1
maxdickinson <max.dickinson58@gmail.com>
2017-01-09 00:56:58 -0800

    daily

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import os
 import math
+#import numpy
 import copy
 pygame.init()
 os.chdir("C:\\Users\\max\\Desktop\\pigwalk")
@@ -47,6 +48,8 @@ class spriteSheet(pygame.sprite.Sprite):
         while self.currentFrame < len(self.sequence):
             row = self.sequence[self.currentFrame] // self.framesOnRow
             col = self.sequence[self.currentFrame] % self.framesOnRow
+            self.row = row
+            self.col = col
             self.images.append(self.sheet.subsurface(col * frameWidth, row * frameHeight, frameWidth, frameHeight))
             self.currentFrame += 1
         self.currentFrame = 0
@@ -58,7 +61,7 @@ class spriteSheet(pygame.sprite.Sprite):
         self.image.set_colorkey(wh)
 
         self.cover = pygame.Surface((self.image.get_rect().size))
-        self.cover.set_colorkey(wh)
+        #self.cover.set_colorkey(wh)
         self.cover.fill((255, 255, 255))
     def speed(self):
         #wrapper
@@ -67,7 +70,7 @@ class spriteSheet(pygame.sprite.Sprite):
         if self.counter == speed:
 
             self.currentFrame = (self.currentFrame + 1) % len(self.sequence)
-
+            #print(self.currentFrame)
         self.counter += 1
         if self.counter > speed:
             self.counter = 0
@@ -84,25 +87,68 @@ class spriteSheet(pygame.sprite.Sprite):
     def moveUp(self,screen):
         #takes care of erasing as well
         screen.blit(self.cover, self.rect)
-        self.rect.y += 10
+        self.rect.y += 2
     def moveDown(self,screen):
-        screen.blit(self.cover, self.rect)
-        self.rect.y -= 10
-    def reset(self):
-        screen.blit(self.cover, self.rect)
-        self.rect.x = self.originx
-        self.rect.y= self.originy
 
-ladder = spriteSheet("ladder.png",400,450,20,150,0,1)
-climber = spriteSheet("climber.png",400,5,19,32,0,4)
-print(climber.image.get_alpha())
+        #screen.blit(self.cover, self.rect)
+        self.rect.y -= 30
+    def reset(self,back):
+        screen.blit(back, self.rect)
+        #self.rect.x = self.originx
+        #self.rect.y= self.originy
+frameHeight = 150
+ladArray = [i for i in range(screen.get_height() // frameHeight)]
+ladders =[]
+for i in ladArray:
+    ladders.append(spriteSheet("ladder.png",600,i*frameHeight,20,150,0,1))
+climber = spriteSheet("climber.png",600,560,19,32,0,2)
+#print(climber.image.get_alpha())
 
+
+vineBlocks = []
 frameHeight = 20
 ladArray = [i for i in range(screen.get_height() // frameHeight)]
-for i in ladArray[0:18]:
-    vineBlocks = spriteSheet("vine_bricks.png",600,i*frameHeight,20,20,i,i+1)
 
-vineBlocks = [spriteSheet("vine_bricks.png",600,i*frameHeight,20,20,i,i+1) for i in ladArray[0:18] ]
+for i in ladArray[0:19]:
+    vineBlocks.append(spriteSheet("vine_bricks.png",600,i*frameHeight,20,20,i,i+1))
+for j in ladArray[0:19]:
+    #print(j)
+    vineBlocks.append(spriteSheet("vine_bricks.png", 600, (i+j) * frameHeight, 20, 20, j, j + 1))
+
+#green bricks has (84,103), want to fill up screen1.w / 2, screen1.h / 2 place at a[0:2]
+gb = spriteSheet("green_bricks.png",0,0,100,80,0,1)
+
+#print((screen1.w / 2) // 100)
+#print((screen1.h / 2) //80)
+print((screen1.w / 2))
+print((screen1.h / 2))
+
+def tileScreen(file,x,y,ww,hh,frameHeight,frameWidth):
+    #must have one dimension of screen evenylly divide into frame dimension
+    gb = []
+    for i in range(int(ww // frameWidth)):
+        for j in range(int(hh //frameHeight)):
+            print(i,j)
+
+            gb.append(spriteSheet(file, x+frameWidth*i, y+frameHeight*j, frameWidth, frameHeight, 0, 1))
+            if j == int((hh) //frameHeight)-1 and (hh-frameHeight* int((hh) // frameHeight)) > 0:
+                gb.append(spriteSheet(file, x+frameWidth*i, y+frameHeight*int((hh) //frameHeight), frameWidth, hh-frameHeight*int((hh) //frameHeight), 0, 1))
+            if i == int((ww) // frameWidth) - 1 and (ww-frameWidth* int((ww) // frameWidth)) > 0:
+                #print(j)
+                gb.append(spriteSheet(file, x+frameWidth * int((ww) // frameWidth), y+frameHeight * j, ww-frameWidth* int((ww) // frameWidth), frameHeight , 0, 1))
+    return gb
+ww = screen1.w / 2
+hh =screen1.h / 2
+frameHeight = 80
+frameWidth = 100
+gb = tileScreen("green_bricks.png",0,0,ww,hh,frameHeight,frameWidth)
+
+sandBrick = tileScreen("sand_brick.png",0,300,ww,hh,50,150)
+#strange bug that brick rects are added to this list
+sandBrick=sandBrick[0::]
+castleBrick = tileScreen('castle_bricks.png' , 300,0,ww,hh,42,75)
+
+longBrick = tileScreen('long_bricks.png' , 300,300,ww,hh,40,100)
 
 
 class TextBlock:
@@ -140,6 +186,10 @@ class Background(pygame.sprite.Sprite):
 
 bgnd1 = Background("background1.png", (0, 0))
 bgnd2 = Background("background2.png", (0, 0))
+bgnd3 = Background("back3.png", (0, 0))
+bgnd4 = Background("back4.png", (0, 0))
+bgnd5 = Background("back5.png", (0, 0))
+
 screen.blit(bgnd1.image, bgnd1.rect)
 
 # 4 RECTANGLES COVERING SCREEN EVENLY
@@ -148,6 +198,10 @@ a = (0, 0, screen1.w / 2, screen1.h / 2)
 b = (screen1.w / 2, 0, screen1.w / 2, screen1.h / 2)
 c = (0, screen1.h / 2, screen1.w / 2, screen1.h / 2)
 d = (screen1.w / 2, screen1.h / 2, screen1.w / 2, screen1.h / 2)
+
+
+
+
 recs = [a, b, c, d]
 recs1 = list(map(pygame.Rect, recs))
 recs3 = list(map(pygame.Rect, recs))
@@ -174,7 +228,7 @@ def drawCircle(rp):
     radius = rp[0]
     pos = rp[1]
     circle = pygame.draw.circle(screen, (0, 255, 0), pos, radius, 1)
-    print(circle)
+    #print(circle)
     return circle
 
 
@@ -212,7 +266,21 @@ bl = [q,r,s,t]
 bl2 =[q,r,s,t]
 index = 0
 index2 = 0
+events = []
+unrun = True
+pxx = []
+pxy = []
+recbri = [gb[0].rect, sandBrick[0].rect, castleBrick[0].rect, longBrick[0].rect]
+recbri2 = [gb[0].rect, sandBrick[0].rect, castleBrick[0].rect, longBrick[0].rect]
+#recbri=[]
+#recbri2 = []
+for i in sandBrick:
+    print(i.rect)
+print("ADSASDASD")
+for i in gb:
+    print(i.rect)
 while True:
+
     x, y = pygame.mouse.get_pos()
 
     # erase
@@ -227,7 +295,7 @@ while True:
 
         if event.type == pygame.QUIT:
             sys.exit()
-        elif start[0:4] == [False]*4 and nextBut.rect.collidepoint(pygame.mouse.get_pos()) and (event.type == pygame.MOUSEBUTTONDOWN):
+        elif start[0:4] == [False]*4 and nextBut.rect.collidepoint(pygame.mouse.get_pos()) and (event.type == pygame.MOUSEBUTTONDOWN) and curr == bgnd1:
 
             start = [True] * 4
             curr = bgnd2
@@ -235,27 +303,45 @@ while True:
             bl = [q ,r ,s ,t]
             recs1 = [a, b, c, d]
         elif event.type == pygame.MOUSEBUTTONDOWN and curr == bgnd2 and index < 4 :
-            bl2[index].colr +=1
+            bl2[index].colr +=5
             if bl2[index].colr == len(colors):
                 cols[index] = True
                 bl2[index].colr =len(colors)-1
                 index +=1
             #makes the other box change colors first
+        elif start[0:4] == [False]*4 and nextBut.rect.collidepoint(pygame.mouse.get_pos()) and (event.type == pygame.MOUSEBUTTONDOWN) and curr == bgnd2:
+            print('ran')
+            for i in range(len(bl2)):
+                bl2[i].colr = 0
+            start = [True,True,True,True]
+            curr = bgnd3
+            recs = [a, b, c, d]
+            bl = [q ,r ,s ,t]
+            recs1 = [a, b, c, d]
+            index2=0
+    screen.blit(curr.image, curr.rect)
+
+    # shrink circle
+
     if curr == bgnd1:
         for i in range(len(px)):
 
             if math.sqrt((px[i] - x) ** 2 + (py[i] - y) ** 2) <= radius and start[i] ==True :
+                events.append("climbdown")
+
                 recs.remove(recs2[i])
                 recs1.remove(recs3[i])
                 print(bl)
-                print(bl2[i])
+                #print(bl2[i])
                 bl.remove(bl2[i])
                 start[i] = False
                 #start[i::] == [True] * (4 - i) and start[0:i] == [False] * i
     elif curr == bgnd2:
 
 
-        if math.sqrt((px[index2] - x) ** 2 + (py[index2] - y) ** 2) <= radius and cols[index2] == True and start[i] == True and index2<4:
+        if math.sqrt((px[index2] - x) ** 2 + (py[index2] - y) ** 2) <= radius and cols[index2] == True and start[index2] == True and index2<4:
+            events.append("climbdown")
+
             recs.remove(recs2[index2])
             recs1.remove(recs3[index2])
             bl.remove(bl2[index2])
@@ -264,40 +350,113 @@ while True:
                 index2 = 3
             else:
                 index2+=1
+    # update background image
+
+    elif curr == bgnd3:
+
+        if len(recbri) ==0:
+            recbri.append(gb.pop().rect)
+            recbri.append(sandBrick.pop().rect)
+            recbri.append(castleBrick.pop().rect)
+            recbri.append(longBrick.pop().rect)
+            unrun = True
+        rpp = list(map(radpos, recbri))
+        if unrun == True:
+            pxx=[]
+            pxy=[]
+            for p in rpp:
+                pxx.append(p[1][0])
+                pxy.append(p[1][1])
+            unrun = False
+            start = [True, True, True, True]
+        #start = [True] * len(pxx)
+        for i in range(len(pxx)):
+            print(pxx[i], pxy[i] , i,radius )
+            print(start[i])
+            if math.sqrt((pxx[i] - x) ** 2 + (pxy[i] - y) ** 2) <= radius and start[i] == True:
+                print(i)
+                events.append("climbdown")
+            #recbri.remove(recbri2[index2])
+                recbri.remove(recbri2[i])
+                w = recbri2[i]
+                try : gb.remove(w)
+                except : ValueError
+                try:
+                    sandBrick.remove(w)
+                except:
+                    ValueError
+                try:
+                    castleBrick.remove(w)
+                except:
+                    ValueError
+                try:
+                    longBrick.remove(w)
+                except:
+                    ValueError
+
+            #recs.remove(recs2[index2])
+            #recs1.remove(recs3[index2])
+            #bl.remove(bl2[index2])
+                start[i] = False
+
 
 
     # circle=[targetingCircle(screen, (int(a[0] + a[2] / 2), int(a[1] + a[3] / 2)), a[2] / 2)]
 
-    # update background image
-    screen.blit(curr.image, curr.rect)
+
     if len(recs) == 0:
         nextBut = createText(300, 300, "Next", 200)
         screen.blit(nextBut.tex,nextBut.rect)
 
     # draw blockers
     color = (0, 0, 0)
+    if curr == bgnd1 or curr == bgnd2:
+        for rec in bl:
+            pygame.draw.rect(screen, (colors[rec.colr].rgb[0]*255 , colors[rec.colr].rgb[1]*255 , colors[rec.colr].rgb[2]*255), rec.coord)
+        # shrink circle
 
-    for rec in bl:
+        for oo in rp:
+            drew2 = [drawCircle2(oo, radius)]
+        if radius > 4:
+            radius -= 3
+        else:
+            radius = int(a[2] / 2)
+    elif curr == bgnd3:
+        #print(len(gb))
+        #print(len(sandBrick))
+        #screen.blit(curr.image, curr.rect) doesnt work because surface on surface??
+        [gb[i].draw(screen) for i in range(len(gb))]
+        [sandBrick[i].draw(screen) for i in range(len(sandBrick))]
+        [castleBrick[i].draw(screen) for i in range(len(castleBrick))]
+        [longBrick[i].draw(screen) for i in range(len(longBrick))]
+        for oo in rpp:
+            drew2 = [drawCircle2(oo, radius)]
 
-        pygame.draw.rect(screen, (colors[rec.colr].rgb[0]*255 , colors[rec.colr].rgb[1]*255 , colors[rec.colr].rgb[2]*255), rec.coord)
-    # shrink circle
-
-    for oo in rp:
-        drew2 = [drawCircle2(oo, radius)]
-    if radius > 4:
-        radius -= 3
-    else:
-        radius = int(a[2] / 2)
+        if radius > 4:
+            radius -= 3
+        else:
+            radius = int(a[2] / 4)
 
 
     [vineBlocks[i].draw(screen) for i in range(len(vineBlocks))]
-    for i in range(4):
-        climber.reset()
-        climber.update(30)
-        climber.draw(screen)
-        pygame.display.update()
-        pygame.display.flip()
-    ladder.draw(screen)
+
+    [ladders[i].draw(screen) for i in range(len(ladders))]
+
+
+
+    if len(events) > 0:
+        #print(events)
+        if events.pop() == "climbdown":
+            climber.update(0)
+            #if climber.currentFrame > len(climber.sequence):
+                #climber.currentFrame = 0
+            climber.moveDown(screen)
+            climber.draw(screen)
+            pygame.display.update()
+            pygame.display.flip()
+            #pygame.time.wait(500)
+
+    climber.draw(screen)
 
     # update
     pygame.display.update()
